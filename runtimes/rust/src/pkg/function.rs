6e804d0761fc18ce7061817c19db6b6a1568567f
ashupednekar <ashutoshpednekar15@gmail.com>
2025-06-08 16:15:14 +0530

    test: added test, working fine locally

--- runtimes/rust/src/pkg/function.rs
+++ runtimes/rust/src/pkg/function.rs
@@ -1,5 +1,7 @@
-use crate::{pkg::state::AppState, prelude::Result};
+use crate::{prelude::Result, pkg::conf::settings, pkg::state::AppState};
+use uuid::Uuid;
 
-pub async fn handler(state: AppState, req_id: Option<&str>) -> Result<Vec<u8>> {
-    Ok("".into())
+pub async fn handler(state: AppState, req_id: Option<&str>) -> Result<Vec<u8>>{
+  tracing::debug!("handler triggered");
+  Ok(Uuid::new_v4().to_string().into())
 }
