0054c044c18b093ae888b3421d7f513be1343bb5
ashupednekar <ashutoshpednekar15@gmail.com>
2025-06-08 18:48:51 +0530

    feat: python runtime working
    locally xD

--- runtimes/python/main.py
+++ runtimes/python/main.py
@@ -1,5 +1,38 @@
-
 import asyncio
+import pygit2
+import os
+
+from pkg.conf import settings
 from pkg.consumer import start_function
+from pkg.state import AppState
+
+import logging
+
+logger = logging.getLogger(__name__)
+log_format = '%(asctime)s [%(levelname)s] - %(message)s'
+date_format = '%Y-%m-%d %H:%M:%S'
+logging.basicConfig(level=logging.DEBUG,
+                    format=log_format,
+                    datefmt=date_format)
 
 
+async def run():
+    state: AppState = await AppState.new()
+    if os.path.exists(f"/tmp/{settings.project}"):
+        pygit2.Repository(f"/tmp/{settings.project}").remotes["origin"].fetch()
+    else:
+        pygit2.clone_repository(
+            url=f"https://github.com/{settings.git_user}/{settings.project}",
+            path=f"/tmp/{settings.project}",
+        )
+    for func in os.listdir(f"/tmp/{settings.project}/functions/py/"):
+        func = func[:-3]
+        if "__" in func:
+            continue
+        logger.info(f"spawning listner for function: {func}")
+        asyncio.create_task(start_function(state, func))
+        await asyncio.sleep(300)
+
+
+if __name__ == "__main__":
+    asyncio.run(run())
--- runtimes/python/pkg/consumer.py
+++ runtimes/python/pkg/consumer.py
@@ -1,4 +1,5 @@
-from typing import Any, Callable
+import sys
+from typing import Any, Callable, List
 import importlib
 import asyncio
 
@@ -8,25 +9,25 @@ from pkg.state import AppState
 from pkg.conf import settings
 
 import logging
+
 logger = logging.getLogger(__name__)
+
 
 async def start_internal(state: AppState):
     consumer = await state.js.pull_subscribe(
-        f"{settings.project}.{settings.environment}.spawn",
-        stream=f"{settings.project}-{settings.environment}"
+        f"{settings.project}.spawn", stream=settings.project
     )
     async for msgs in consumer.fetch():
         msg: Msg = msgs[0]
         # TODO: refresh code from repo
-        func_name = msg.data
-        function = importlib.import_module(f"pkg.handlers.{func_name}").handle
-        asyncio.create_task(start_function(state, function))
+        asyncio.create_task(start_function(state, msg.data))
         await msg.ack()
 
 
 async def consume(state: AppState, consumer: Any, handler: Callable):
     logger.debug("waiting for messages...")
-    async for msgs in consumer.fetch():
+    while True:
+        msgs: List[Msg] = await consumer.fetch(timeout=None)
         msg: Msg = msgs[0]
         logger.info("received event")
         await msg.ack()
@@ -36,26 +37,29 @@ async def consume(state: AppState, consumer: Any, handler: Callable):
             res: bytes = await handler(state, req_id)
             logger.debug("handler run complete")
             await state.nc.publish(
-                f"{settings.project},{function.__name__}.res.py.{req_id}",
-                res
+                f"{settings.project}.{handler.__name__}.res.py.{req_id}", res
             )
 
+
 async def start_function(state: AppState, name: str):
-    module = importlib.import_module(f"/tmp/{settings.project}/functions/py/{name}.py")
-    if module.hasattr("handle"):
+    spec = importlib.util.spec_from_file_location(
+        name, f"/tmp/{settings.project}/functions/py/{name}.py"
+    )
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[name] = module
+    spec.loader.exec_module(module)
+    handler = module.handle
+    setattr(handler, "__name__", name)
+    if hasattr(module, "handle"):
         consumer = await state.js.pull_subscribe(
-            f"{settings.project}.{name}.exec.py.*",
-            stream=settings.project
+            f"{settings.project}.{name}.exec.py.*", stream=settings.project
         )
-        async for msgs in consumer.fetch():
-            msg: Msg = msgs[0]
-            await msg.ack()
+        await consume(state, consumer, module.handle)
 
 
 class TestRuntime(IsolatedAsyncioTestCase):
-
     PROJECT = "projone"
     NAME = "one"
+
     async def test_start_fn():
         state = AppState()
-        
