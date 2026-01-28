ef2672ccf82a23a56470ea1bbc176b643fb55ba7
Ashu Pednekar <ashupednekar49@gmail.com>
2026-01-28 12:51:58 +0530

    chore: ci updated

--- build/runtimes/Dockerfile.go
+++ build/runtimes/Dockerfile.go
@@ -9,10 +9,7 @@ WORKDIR /app
 COPY . .
 RUN ls
 
-#RUN curl -H "Authorization: Bearer $GIT_TOKEN" https://raw.githubusercontent.com/$GIT_USER/$PROJECT/main/functions/rs/$NAME.rs -o pkg/function.go
 RUN go build cmd/main.go
-
-RUN ls
 
 FROM scratch 
 COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
