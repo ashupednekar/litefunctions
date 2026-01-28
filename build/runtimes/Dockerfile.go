014c222e0303bfa6eaf179b90a4f41d7d2984a59
Ashu Pednekar <ashupednekar49@gmail.com>
2026-01-28 13:13:37 +0530

    build: fixed go dockerfile

--- build/runtimes/Dockerfile.go
+++ build/runtimes/Dockerfile.go
@@ -1,20 +1,24 @@
-FROM golang AS builder
+FROM golang:1.24-alpine AS builder
+
+RUN apk add --no-cache git ca-certificates
 
 ARG GIT_TOKEN
 ARG GIT_USER=lwsrepos
 ARG PROJECT
-ARG NAME 
+ARG NAME
 
 WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
 COPY . .
-RUN ls
 
-RUN go build cmd/main.go
+RUN CGO_ENABLED=0 GOOS=linux go build -a -ldflags="-w -s" -o /app/main ./cmd/main.go
 
-FROM scratch 
+FROM scratch
+
 COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
-
 COPY --from=builder /app/main /func
 
 CMD ["/func"]
-
