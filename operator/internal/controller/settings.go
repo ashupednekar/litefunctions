0d3fe8c78bb730648a9b9a8b3d2e8d5a4d1605a5
ashupednekar <ashutoshpednekar15@gmail.com>
2026-01-30 21:12:08 +0530

    chore: updated operator rbac to include function verbs, and added isActive,deprovisionTime to spec

--- operator/internal/controller/settings.go
+++ operator/internal/controller/settings.go
@@ -2,20 +2,22 @@ package controller
 
 import (
 	"sync"
+	"time"
 
 	"github.com/go-logr/logr"
 	"go-simpler.org/env"
 )
 
 type Settings struct {
-	Registry            string `env:"REGISTRY" default:"ghcr.io"`
-	VcsUser             string `env:"VCS_USER" default:"lwsrepos"`
-	PullSecret          string `env:"PULL_SECRET" default:"ghcr-secret"`
-	DbSecretName        string `env:"DB_SECRET_NAME" default:"litefunctions-pguser-litefunctions"`
-	DbSecretKey         string `env:"DB_SECRET_KEY" default:"pgbouncer-uri"`
-	RedisUrl            string `env:"REDIS_URL" default:"redis://litefunctions-valkey-cluster:6379"`
-	NatsUrl             string `env:"NATS_URL" default:"nats://litefunctions-nats:4222"`
-	DeprovisionDuration string `env:"DEPROVISION_DURATION" default:"2m"`
+	Registry            string        `env:"REGISTRY" default:"ghcr.io"`
+	VcsUser             string        `env:"VCS_USER" default:"lwsrepos"`
+	PullSecret          string        `env:"PULL_SECRET" default:"ghcr-secret"`
+	DbSecretName        string        `env:"DB_SECRET_NAME" default:"litefunctions-pguser-litefunctions"`
+	DbSecretKey         string        `env:"DB_SECRET_KEY" default:"pgbouncer-uri"`
+	RedisUrl            string        `env:"REDIS_URL" default:"redis://litefunctions-valkey-cluster:6379"`
+	NatsUrl             string        `env:"NATS_URL" default:"nats://litefunctions-nats:4222"`
+	DeprovisionDuration string        `env:"DEPROVISION_DURATION" default:"2m"`
+	KeepWarmDuration    time.Duration `env:"KEEP_WARM_DURATION" default:"5m"`
 }
 
 var (
